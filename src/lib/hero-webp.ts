f9546ed66a4090d506e81613e23547708da0d4c0
GooseyPrime <brandonlane1977@outlook.com>
2026-09-15 18:47:05 -0400

    Globe plate assembler imports ten 20k chunks

--- src/lib/hero-webp.ts
+++ src/lib/hero-webp.ts
@@ -3,5 +3,10 @@ import { P1 } from './hero-p1';
 import { P2 } from './hero-p2';
 import { P3 } from './hero-p3';
 import { P4 } from './hero-p4';
+import { P5 } from './hero-p5';
+import { P6 } from './hero-p6';
+import { P7 } from './hero-p7';
+import { P8 } from './hero-p8';
+import { P9 } from './hero-p9';
 
-export const HERO_WEBP_B64 = P0 + P1 + P2 + P3 + P4;
+export const HERO_WEBP_B64 = P0 + P1 + P2 + P3 + P4 + P5 + P6 + P7 + P8 + P9;
