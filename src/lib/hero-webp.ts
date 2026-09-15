401226288fa948ed3f282fc34c34dfe11bab3a82
GooseyPrime <brandonlane1977@outlook.com>
2026-09-15 18:41:02 -0400

    Hero: gold globe plate, slightly enlarged, copy on the dark left

---
+++ src/lib/hero-webp.ts
@@ -0,0 +1,7 @@
+import { P0 } from './hero-p0';
+import { P1 } from './hero-p1';
+import { P2 } from './hero-p2';
+import { P3 } from './hero-p3';
+import { P4 } from './hero-p4';
+
+export const HERO_WEBP_B64 = P0 + P1 + P2 + P3 + P4;
